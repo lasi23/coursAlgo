24a65bd82544a5c3702facd30c1ca1fa180f3fce
lasi23 <lsilvere39@gmail.com>
2025-08-26 16:00:58 +0200

    Ajout de fichiers pour les exercices de dessin avec la tortue, y compris les scripts et le fichier HTML.

---
+++ exercices/exTurtle/js/agloblocs.js
@@ -0,0 +1,517 @@
+/*//ex A1
+
+//paramètre
+setPos(300,300);
+changeColor(color.red)
+setLineWidth(10);
+
+// Dessin
+forward(200);
+faceRight();
+forward(100);
+*/
+
+
+
+
+/*
+//ex A2
+
+//Paramètre
+setPos(300,300);
+changeColor(color.yellow);
+setLineWidth(10);
+
+//dessin
+faceLeft();
+forward(100);
+faceDown();
+forward(100);
+faceRight();
+forward(200);
+faceUp();
+forward(200);
+*/
+
+
+/*
+//ex A3
+
+//Paramètre
+setPos(300,300);
+changeColor(color.yellow);
+setLineWidth(10);
+
+//dessin
+forward(100);
+faceUp();
+forward(200);
+faceDown();
+forward(100);
+faceLeft();
+forward(100);
+faceRight();
+forward(200);
+*/
+
+
+/*
+//ex A4
+
+//Paramètre
+setPos(200,500);
+changeColor(color.green);
+setLineWidth(10);
+faceRight();
+
+//dessin
+forward(200);
+arcLeft(100,180);
+forward(100);
+arcRight(100,180);
+forward(200);
+*/
+
+
+/*
+// ex A5
+
+//Paramètre
+setPos(300,200);
+changeColor(color.fuchsia);
+setLineWidth(10);
+faceRight();
+
+//dessin
+arcRight(200,360);
+arcRight(100,180);
+arcLeft(100,180);
+*/
+
+
+/*
+//ex 06
+
+//Paramètre
+setPos(300,500);
+changeColor(color.blue);
+setLineWidth(10);
+faceUp();
+
+//dressin
+forward(100);
+up();
+forward(100);
+down();
+forward(200);
+*/
+
+
+/*
+//ex 07
+
+//Paramètre
+setPos(200,300);
+changeColor(color.green);
+setLineWidth(10)
+
+//Dessin
+forward(200);
+arcLeft(100,180);
+forward(100);
+up();
+forward(100);
+down();
+forward(200);
+arcLeft(100,180);
+forward(100);
+*/
+
+
+
+/*
+//ex 09
+
+//parametre
+setPos(200,500);
+changeColor(color.blue);
+faceRight();
+setLineWidth(10);
+
+//dessin
+forward(200);
+faceLeft();
+arcRight(50,180);
+arcLeft(50,180);
+forward(200);
+arcLeft(50,180);
+arcRight(50,180)
+*/
+
+
+
+/*
+//ex 10
+
+//parametre
+setPos(300,300);
+changeColor(color.red);
+faceRight();
+setLineWidth(10);
+
+//dessin
+arcLeft(50,180);
+changeColor(color.green);
+arcLeft(100,180);
+changeColor(color.yellow);
+arcLeft(200,180);
+*/
+
+
+
+
+//---------------SERIE B---------------------------------
+
+//FUNCTION
+//for(let i = 0 ; i < 4 ; i++)
+
+
+/*
+//ex B1
+
+//parametre
+setPos(150,150);
+setLineWidth(10);
+faceRight();
+changeColor(color.green);
+
+//dessin
+for(let i = 0 ; i < 4 ; i++){
+    forward(50);
+    faceDown();
+    forward(50);
+    faceRight();
+}
+*/
+
+
+/*
+//ex B2
+
+//paramètre
+setPos(100,300);
+setLineWidth(10);
+changeColor(color.red);
+faceUp();
+
+
+//dessin
+for(let i = 0 ; i < 4; i++){
+    forward(50);
+    faceRight();    
+    forward(50);
+    faceDown();
+    forward(50);
+    faceRight();
+    forward(50);
+    faceUp();
+
+    */
+
+
+
+/*
+//ex B3
+
+//paramètre
+setPos(250,150);
+setLineWidth(10);
+faceRight();
+changeColor(color.fuchsia)
+
+//dessin
+for(let i = 0; i < 4; i++){
+forward(100);
+right(90)
+}
+*/
+
+
+/*
+//ex B4
+
+// parametre
+setPos(300,300);
+setLineWidth(10);
+changeColor(color.blue);
+faceRight();
+
+//dessin
+for(let i = 0; i < 3; i++){
+forward(100);
+left(180-60);
+}
+*/
+
+
+/*
+//ex B5
+
+//parametre
+setPos(300,400);
+setLineWidth(10);
+changeColor(color.yellow);
+faceUp();
+
+//dessin
+
+forward(100);
+changeColor(color.red);
+for(let i = 0; i < 3; i++){
+    forward(50);
+    right(180-60);
+}
+*/
+
+
+/*
+// ex B6
+
+//prametre
+setPos(100,300);
+setLineWidth(10);
+changeColor(color.red);
+faceRight();
+
+//dessin
+for(let i= 0; i < 2; i++){
+forward(50);
+faceUp();
+forward(50);
+faceRight()
+}
+changeColor(color.green);
+for(let i = 0; i < 3; i++){
+    forward(50);
+    right(90);
+    forward(50);
+    left(90);
+}
+*/
+
+
+/*
+//ex B7
+
+//Paramètre
+setPos(200,200)
+setLineWidth(10);
+changeColor(color.yellow);
+faceRight();
+
+//dessin
+for(let i = 0; i < 3; i++){
+    forward(100);
+    left(180-60);
+}
+faceDown();
+for(let i = 0; i < 3; i++){
+    forward(100);
+    left(90)
+}
+*/
+
+
+/*
+//ex B8
+
+//Paramètre
+setPos(300,300)
+setLineWidth(10);
+changeColor(color.yellow);
+faceRight();
+
+//dessin
+for(let i= 0; i <8; i++){
+forward(200);
+right(180-45)
+}
+*/
+
+
+/*
+//ex B9
+
+//Paramètre
+setPos(100,300);
+setLineWidth(10);
+changeColor(color.fuchsia);
+faceRight();
+
+//dessin
+forward(50);
+up();
+forward(50);
+down();
+for(let i = 0; i < 3; i++){
+    forward(100);
+    right(180-60);
+}
+*/
+
+
+
+//----------------------  SERIE C   -----------------------------
+
+
+/*
+//ex C4
+
+//Paramètre
+setPos(30,250);
+setLineWidth(5);
+changeColor(color.aqua);
+faceRight();
+
+//dessin
+for(let i = 0; i < 4; i++){
+    for(let j = 0; j < 3; j++){
+        forward(50);
+        right(180-60)
+    }
+    forward(50)
+}
+*/
+
+
+/*
+//ex C2
+
+//paramètre
+setPos(300,550);
+setLineWidth(5);
+changeColor(color.fuchsia);
+faceUp();
+
+//dessin
+for(let i = 0; i < 5; i++){
+    for(let j = 0; j < 4; j++){
+        down();
+        forward(50);
+        right(90);
+        up();
+    }
+    forward(100);
+}
+*/
+
+
+
+/*
+//ex C3
+
+//Paramètre
+setPos(20,200);
+setLineWidth(5);
+shiftColor(0.3);
+faceRight();
+
+//dessin
+for(let i = 0; i < 3; i++){
+    for(let j = 0; j < 8; j++){
+        forward(100);
+        left(180-45);
+    }
+    up();
+    forward(200);
+    down();
+    shiftColor(0.1);
+}
+*/
+
+
+/*
+//ex C4
+
+//Paramètre
+setPos(20,250);
+setLineWidth(5);
+faceRight();
+
+//Dessin
+for(let i = 0; i < 5; i++){
+    for(let j = 0; j < 3; j++){
+        changeColor(color.red);
+        forward(50);
+        left(180-60);
+    }
+    for( let j= 0; j < 4; j++){
+        changeColor(color.yellow);
+        forward(50);
+        right(90)
+    }
+    up();
+    forward(100);
+    down();
+}
+*/
+
+
+/*
+//ex C5
+
+//Paramètre
+setPos(300,300);
+setLineWidth(5);
+faceRight();
+shiftColor(0.2);
+
+//dessin
+for(let i = 0; i < 4; i++){
+    for(let j = 0; j < 8; j++){
+        forward(100);
+        left(180-45);
+    }
+    right(90);
+    shiftColor(0.2);
+}
+*/
+
+
+/*
+//ex C6
+
+//Paramètre
+setPos(250,400)
+setLineWidth(5);
+changeColor(color.yellow);
+faceRight();
+
+//dessin
+for(let i = 0; i < 3; i++){
+    for(let j = 0; j < 5; j++){
+        forward(50);
+        left(180-60);
+    }
+    right(120)
+}
+*/
+
+
+
+//ex C8
+
+//Paramètre
+setPos(200,400);
+setLineWidth(5);
+changeColor(color.yellow);
+faceRight();
+
+//dessin
+for(let i = 0; i < 8; i++){
+    for(let j = 0; j < 4; j++){
+        forward(75);
+        right(180-60);
+    }
+    left(180-15)
+}
---
+++ exercices/exTurtle/js/turtle.js
@@ -0,0 +1,252 @@
+﻿"use strict";
+// JavaScript statements
+// =====================================================================================
+let color = {
+	white: "#ffffff",
+	black: "#000000",
+	red: "#ff0000",
+	green: "#00ff00",
+	blue: "#0000ff",
+	yellow: "#ffff00",
+	fuchsia: "#ff00ff",
+	aqua: "#00ffff"
+};
+
+let turtle = {
+	x: 0,
+	y: 0,
+	angleInRadians: 0,
+	penDown: true,
+	penColor: "#000000",
+	lineWidth: 2,
+	col:0
+};
+
+let canvas = document.getElementById('myDrawing');
+let cursorCanvas = document.getElementById('turtleCanvas');
+
+if (canvas && canvas.getContext) { // does the browser support 'canvas'?
+	turtle.ct = canvas.getContext("2d"); // get drawing context
+	turtle.ct.fillStyle = "#888888";
+    turtle.ct.fillRect(0,0,600,600);
+    turtle.cursorCt = cursorCanvas.getContext("2d");
+    turtle.cursorCt.fillStyle = "#FF00FF";
+    //turtle.cursorCt.fillRect(0, 0, 100, 100);
+} else {
+	alert('You need a browser which supports the HTML5 canvas!');
+}
+
+// =====================================================================================
+
+turtle.updateCursorPoints = function() {
+    turtle.cursorPoints = [];
+    let a = -turtle.angleInRadians;
+    for(let [x,y] of [[-15,-10], [-2,0], [-5, -30], [5, -30], [2,0], [15,-10], [0,20]]) {
+        [x,y] = [Math.cos(a)*x - Math.sin(a)*y, Math.sin(a)*x + Math.cos(a)*y];
+        turtle.cursorPoints.push([x,y]);
+    }
+};
+turtle.updateCursorPoints();
+
+turtle.drawCursor = function() {
+    this.cursorCt.clearRect(0, 0, cursorCanvas.width, cursorCanvas.height);
+    this.cursorCt.beginPath();
+    this.cursorCt.moveTo(this.x+this.cursorPoints[this.cursorPoints.length-1][0], this.y+this.cursorPoints[this.cursorPoints.length-1][1]);
+    for(let [x,y] of this.cursorPoints) {
+        this.cursorCt.lineTo(this.x + x, this.y + y);
+    }
+    this.cursorCt.lineWidth = 3;
+    this.cursorCt.strokeStyle = "black";//this.penColor;
+    this.cursorCt.stroke();
+};
+
+turtle.forward = function (length) {
+	let x0 = this.x,
+		y0 = this.y;
+	this.x += length * Math.sin(this.angleInRadians);
+	this.y += length * Math.cos(this.angleInRadians);
+	if (this.ct) {
+		if (this.penDown) {
+			this.ct.beginPath();
+			this.ct.lineWidth = this.lineWidth;
+			this.ct.strokeStyle = this.penColor;
+			this.ct.moveTo(x0, y0);
+			this.ct.lineTo(this.x, this.y);
+			this.ct.stroke();
+		}
+	} else {
+		this.ct.moveTo(this.x, this.y);
+	}
+
+    turtle.drawCursor();
+
+	return this;
+};
+
+turtle.arcLeft = function(radius, arc) {
+	let x0 = this.x,
+		y0 = this.y;
+	let theta0 = this.angleInRadians;
+	let xc = x0 + radius*Math.cos(theta0);
+	let yc = y0 - radius*Math.sin(theta0);
+	let startAngle = theta0 - Math.PI;
+	arc = arc * Math.PI / 180;
+	let endAngle = startAngle + arc;
+	
+	if(arc < 0) {
+		endAngle = Math.PI - theta0;
+		startAngle = endAngle + arc;
+	}
+	
+	if(this.penDown) {
+		this.ct.beginPath();
+		this.ct.lineWidth = this.lineWidth;
+		this.ct.strokeStyle = this.penColor;
+		for(let angle = startAngle ; angle <= endAngle ; angle += 0.001) {
+			let x = xc + radius*Math.cos(angle);
+			let y = yc - radius*Math.sin(angle);
+			this.ct.lineTo(x,y);
+		}
+		this.ct.stroke();
+	}
+	
+	let angle = endAngle;
+	if(arc < 0) {
+		angle = startAngle;
+	}
+	this.x = xc + radius*Math.cos(angle);
+	this.y = yc - radius*Math.sin(angle);
+	this.angleInRadians += arc;
+    turtle.updateCursorPoints();
+    turtle.drawCursor();
+	
+	return this;
+}
+
+turtle.arcRight = function(radius, arc) {
+	let x0 = this.x,
+		y0 = this.y;
+	let theta0 = this.angleInRadians;
+	let xc = x0 - radius*Math.cos(theta0);
+	let yc = y0 + radius*Math.sin(theta0);
+	let startAngle = theta0;
+	arc = arc * Math.PI / 180;
+	let endAngle = startAngle - arc;
+	
+	if(arc < 0) {
+		endAngle = theta0;
+		startAngle = endAngle - arc;
+	}
+	
+	if(this.penDown) {
+		this.ct.beginPath();
+		this.ct.lineWidth = this.lineWidth;
+		this.ct.strokeStyle = this.penColor;
+		for(let angle = startAngle ; angle >= endAngle ; angle -= 0.001) {
+			let x = xc + radius*Math.cos(angle);
+			let y = yc - radius*Math.sin(angle);
+			this.ct.lineTo(x,y);
+		}
+		this.ct.stroke();
+	}
+	
+	let angle = endAngle;
+	if(arc < 0) {
+		angle = startAngle;
+	}
+	this.x = xc + radius*Math.cos(angle);
+	this.y = yc - radius*Math.sin(angle);
+	this.angleInRadians -= arc;
+	
+    turtle.updateCursorPoints();
+    turtle.drawCursor();
+
+	return this;
+}
+
+turtle.backward = function (length) {
+	this.forward(-length);
+    turtle.drawCursor();
+	return this;
+};
+
+turtle.left = function (angleInDegrees) {
+	this.angleInRadians += angleInDegrees * Math.PI / 180.0;
+    turtle.updateCursorPoints();
+    turtle.drawCursor();
+	return this;
+};
+
+turtle.right = function (angleInDegrees) {
+	this.left(-angleInDegrees);
+    turtle.updateCursorPoints();
+    turtle.drawCursor();
+	return this;
+};
+
+
+turtle.angle = function () {
+	return this.angleInRadians * 180.0 / Math.PI;
+};
+
+turtle.setAngle = function (angle) {
+    this.angleInRadians = angle;
+    turtle.updateCursorPoints();
+    turtle.drawCursor();
+}
+
+turtle.changeColor = function (col) {
+	turtle.penColor = col;
+}
+
+turtle.down = function () {
+	turtle.penDown = true;
+}
+
+turtle.up = function () {
+	turtle.penDown = false;
+}
+
+turtle.setPos = function (x,y) {
+	turtle.x = x;
+	turtle.y = y;
+    turtle.drawCursor();
+}
+
+turtle.setLineWidth = function(n) {
+	turtle.lineWidth = n;
+}
+
+
+function colorScale(n) {
+    let k = Math.floor((n%(6*255))/255);
+    let r = 0, g = 0, b = 0;
+    if(k == 0) {
+        r = 255;
+        g = n%255;
+    } else if(k == 1) {
+        r = 255-n%255;
+        g=255;
+    } else if(k == 2) {
+        g = 255;
+        b = n%255;
+    } else if(k == 3) {
+        g = 255-n%255;
+        b = 255;
+    } else if(k == 4) {
+        r = n%255;
+        b = 255;
+    } else {
+        r = 255;
+        b = 255-n%255;
+    }
+    let c= r*256*256+g*256+b;
+    let col = "#" + c.toString(16).padStart(6,'0');
+    return col;
+}
+
+turtle.shiftColor = function(x) {
+    let n = x*255*6;
+    turtle.col = (turtle.col+n)%(255*6);
+    turtle.changeColor(colorScale(Math.round(turtle.col)));
+}
