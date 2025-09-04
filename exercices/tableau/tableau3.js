00784155efa634ab5969a5271da3742ad9b37019
lasi23 <lsilvere39@gmail.com>
2025-09-04 17:00:01 +0200

    Mise à jour des fichiers HTML et JavaScript : suppression des scripts inutilisés et ajout d'un nouveau fichier pour simuler des combats entre personnages.

---
+++ exercices/tableau/tableau3.js
@@ -0,0 +1,91 @@
+// Le but de cet exercice est de simuler des combats entre des personnages
+// Un personnage sera représenté par un nom, un nombre de points de vie, un nombre de points de mana, une force
+
+// 1) Comment faire pour stocker toutes ces informations dans une seule variable ?
+
+// **************   dans un tableau ***********************
+
+const NAME = 0;
+const HP = 1;
+const mana = 2;
+const force = 3;
+
+
+// 2) Créer une variable firstPlayer et stocker dedans le personnage "Nico Minoru" qui a 50 points de vie, 100 points de mana, et 10 de force.
+let firstPlayer = ["Nico Minoru", 50, 100, 10];
+                            //    pv  mana  force
+
+
+// 3) Créer une variable secondPlayer et stocker dedans le personnage "Spiderman" qui a 120 points de vie, 0 point de mana, et 50 de force.
+let secondPlayer = ["Spiderman", 120, 0, 50];
+
+
+// 4) Donner 25 points de vie supplémentaires au premier personnage.
+firstPlayer[1] += 25 ;
+console.log(firstPlayer);
+
+
+// 5) Créer une fonction displayInfo qui reçoit un personnage en entrée et qui affiche dans la console un message à propos de ce personnage (cf en dessous de la fonction).
+let pointPV;
+let pointMana;
+let pointForce;
+
+function displayInfo(player) {
+    const pointPV = player[HP] > 0 ? "points" : "point";
+    const pointMana = player[mana] > 0 ? "points" : "point";
+    const pointForce = player[force] > 0 ? "points" : "point";
+    
+    return console.log(player[0],"a, ", player[HP], pointPV, "de pv, ",player[mana], pointMana, " de mana, ", player[force], pointForce, " de force.");
+}                                 
+
+
+
+// Par exemple, 
+displayInfo(firstPlayer);
+// devra afficher :
+// Nico Minoru a 50 points de vie, 100 points de mana et 10 points de force.
+
+console.log("6");
+
+// 6) Trouver l'erreur de français dans la fonction précédente (:innocent:) en la testant avec le second personnage.
+console.log(displayInfo(secondPlayer));
+
+
+
+
+
+
+// 7) Corriger la fonction pour faire disparaître cette erreur.
+
+// **********       return au 5      *********************
+
+
+
+
+// https://legendary-digital-network-assets.s3.amazonaws.com/wp-content/uploads/2021/11/12201506/clean_259.jpg
+// 8) Le deuxième personnage a perdu 3 de vie. Décrémenter la variable correspondante.
+
+// 9) Vérifier que
+displayInfo(secondPlayer);
+// affiche bien : "Spiderman a 117 points de vie, 0 point de mana, et 50 de force."
+
+// 10) Écrire une fonction directHit qui reçoit deux personnages en entrée et qui fait perdre des points de vie au second. Le nombre de points de vie perdus sera égal à la force du premier personnage.
+function directHit(player1, player2) {
+	
+}
+
+// 11) Utiliser la fonction précédente pour que le second personnage tape le premier personnage.
+
+// 12) Le second personnage boit une potion de soin ! Ajouter 3 points de vie à Spiderman.
+
+// 13) Le premier personnage va boire plusieurs potions.
+let randomNumber = Math.floor(Math.random()*8)+2;
+console.log("Nico Minoru s'apprête à boire",randomNumber,"potions de vie !");
+// TODO
+displayInfo(firstPlayer);
+
+// 14) Écrire une fonction qui reçoit deux personnages A et B en entrée et qui fait lancer un sort à A pour attaquer B
+// Cette fonction doit :
+// - vérifier que le personnage a au moins 10 points de mana ; si ce n'est pas le cas, la fonction est terminée
+// - faire perdre 10 points de mana à A
+// - faire perdre 25 points de vie à B
--- exercices/tableau/tableaux2.js
+++ exercices/tableau/tableaux2.js
@@ -46,28 +46,92 @@ console.log(takeTheBig(tab1));
 
 
 // [Plus difficile - bonus] Écrire une fonction qui prend en entrée un tableau de nombres positifs et qui retourne la deuxième plus grande valeur du tableau.
+console.log("******************");
+
+
+
 
 // Écrire une fonction qui prend en entrée un tableau et un nombre et qui retourne le nombre de fois que ce nombre apparaît dans le tableau.
-function takeTheEqual(tableau){
-    let theEqual = tableau[0];
+console.log(tab1);
+function takeTheEqual(tableau, n){
+    let count = 0;
     for(let i = 0; i < tableau.length; i++){
-        if(tableau[i] < theEqual && tableau[i] > theEqual ){
-            theEqual  = tableau[i];
+        if(tableau[i] === n){
+            count++;
         }
     }
-    return theEqual
+    return count;
 }
-console.log(takeTheEqual(tab1));
+console.log(takeTheEqual(tab1,108));
+
+
+
+
 // Écrire une fonction qui prend en entrée un tableau et un nombre et qui retourne true si le nombre existe dans le tableau, false sinon.
+console.log("******");
+function takeTheEqualB(tableau, n){
+    for(let i = 0; i < tableau.length; i++){
+        if(tableau[i] === n){
+            return true;
+        }
+    }
+    return false;
+}
+console.log(takeTheEqualB(tab1,5));
+
+
+
 
 // [Bonus] Suite de l'exo : on **sait** que le tableau reçu est trié (on ne le vérifie pas). Comment exploiter cette information pour améliorer la recherche d'un élément ?
 
-// Créer un tableau qui contient [1,2,3,...,7777].
 
-// Créer un tableau qui contient [10,20,30,...,77770].
+// Créer un tableau qui contient [1,2,3,...,7777].
+let bestTab = [];
+for(let i = 1; i <= 7777; i++){
+    bestTab.push(i);
+}
+console.log(bestTab);
+
+
+
+
+
+// Créer un tableau qui contient [10,20,30,...,77770]
+// 
+let bestTabB = [];
+for(let i = 10; i <= 77770; i+=10){
+    bestTabB.push(i);
+}
+console.log(bestTabB);
+
+
+
 
 // En se servant du tableau précédent, créer un tableau qui contient [5,10,15,...,38885].
+let bestTabC = [];
+for(let i = 5; i <= 38885; i+=5){
+    bestTabC.push(i);
+}
+console.log(bestTabC);
+
+
 
 // Écrire une fonction qui prend un tableau en entrée et qui en supprime les dernières valeurs, tant qu'elles sont inférieures à 50.
+let tabDestroy = ["boom",1,12,22,32,41];
+let bigDestroy = [1,45,88,54,23,-100,12];
+function destroy(tableau){
+while(tableau[tableau.length-1] < 50){
+    tabDestroy.pop();
+    }
+    return tableau
+}
+// console.log(destroy(tabDestroy));
+console.log(destroy(bigDestroy));
+
+
+
+
+
 // console.log(leNomDeVotreFonction([1,45,88,54,23,-100,12])); // doit afficher [1,45,88,54]
+
 
